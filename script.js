84d7484ec26d1d2d140b9620c50e09c04536e631
Sarah Duban <sarahgrut@hotmail.fr>
2024-02-22 07:41:10 +0100

    ajout d'un script

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("load",function(){
+
+    var container=document.getElementById("container");
+    container.style.backgroundColor="crimson";
+    container.style.height="300px";
+    container.style.width="200px";
+    container.style.margin="auto";
+
+});
