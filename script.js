01ff724808975cc195d1d34bc1241b02d1fb94a6
Sarah Duban <sarahgrut@hotmail.fr>
2024-02-24 16:09:20 +0100

    commit des changements

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 window.addEventListener("load",function(){
 
-    var container=document.getElementById("container");
-    container.style.backgroundColor="crimson";
-    container.style.height="300px";
-    container.style.width="200px";
-    container.style.margin="auto";
+    var container=document.getElementsByClassName('container');
+    for (var i=0;i<container.length;i++){
+        container[i].style.backgroundColor="crimson";
+        container[i].style.width="300px";
+        container[i].style.height="300px";
+        container[i].style.margin="20px";
+    }
 
 });
