b2b48e53c6fd5578b63f15361da5406d93b38c9f
Sarah Duban <sarahgrut@hotmail.fr>
2024-02-27 06:41:35 +0100

    entraînement à GIT

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-    var img=document.getElementsByClassName('diapoImg');
-    var imgVisible=0;
+var img=document.getElementsByClassName('diapoImg');
+var imgVisible=0;
 
 function changerImage(){
-        img[imgVisible].classList.remove('visible');
-        imgVisible=(imgVisible+1)%img.length;
-        img[imgVisible].classList.add('visible');
-    }
-    img[0].classList.add('visible');
-document.getElementById('suivant').onclick=changerImage;
+img[imgVisible].classList.remove('visible');
+imgVisible=(imgVisible+1)%img.length;
+img[imgVisible].classList.add('visible');
+
+}
+document.getElementById('next').onclick=changerImage;
 changerImage();
