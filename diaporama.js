d91476fcfda2b6c5fd8e9b298b650171b3b580d1
Sarah Duban <sarahgrut@hotmail.fr>
2024-03-01 10:32:37 +0100

    changements

--- diaporama.js
+++ diaporama.js
@@ -1,12 +1,11 @@
-var photo=document.getElementsByClassName('photo');
-var next=document.getElementById('next');
-var imageVisible=0;
+var photos=document.getElementsByClassName('photos');
+var photoVisible=0;
 
 function changerImage(){
-    photo[imageVisible].classList.remove('visible');
-imageVisible=(imageVisible+1) % photo.length;
-photo[imageVisible].classList.add('visible');
+    photos[photoVisible].classList.remove('visible');
+    photoVisible=(photoVisible+1)%photos.length;
+    photos[photoVisible].classList.add('visible');
 }
-photo[0].classList.add('visible');
-next.onclick=changerImage;
+photos[photoVisible].classList.add('visible');
+document.getElementById('next').onclick=changerImage;
 changerImage();
